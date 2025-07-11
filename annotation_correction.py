f6c53ab6b067f16721fba7561c8f67738b845210
BrunoLaurela <142028548+BrunoLaurela@users.noreply.github.com>
2025-07-11 22:43:24 +0000

    update gauth

--- annotation_correction.py
+++ annotation_correction.py
@@ -31,7 +31,8 @@ path_to_json_key = 'anotadorstreamlit.json'
 
 def setup_drive(session_state):
     try:
-        drive = get_drive(path_to_json_key)
+        #drive = get_drive(path_to_json_key)
+        drive = get_drive_oauth()
     except Exception:
         st.error("⚠️ Hubo un problema al inicializar la conexión con Google Drive. Notifica al administrador.")
         return -1
